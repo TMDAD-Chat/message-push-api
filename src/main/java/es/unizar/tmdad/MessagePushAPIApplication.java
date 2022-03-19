79a6c1932aa58828327dc8518c8c9fad6ee6254c
lAngel <angelcanal97@gmail.com>
2022-03-19 17:58:49 +0100

    US-1. Initial design

---
+++ src/main/java/es/unizar/tmdad/MessagePushAPIApplication.java
@@ -0,0 +1,13 @@
+package es.unizar.tmdad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessagePushAPIApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessagePushAPIApplication.class, args);
+	}
+
+}
