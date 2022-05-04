d4ab8224494d2a67bb9eda1ccefb9e764f7d2155
gmrocca <gmrocca@hotmail.it>
2022-05-04 05:30:34 +0200

    bug fix

--- src/main/java/es/unizar/tmdad/controller/impl/RoomControllerImpl.java
+++ src/main/java/es/unizar/tmdad/controller/impl/RoomControllerImpl.java
@@ -1,13 +1,20 @@
 package es.unizar.tmdad.controller.impl;
 
 import es.unizar.tmdad.controller.RoomController;
+import es.unizar.tmdad.repository.MessageTimestampRepository;
+import es.unizar.tmdad.repository.RoomRepository;
+import es.unizar.tmdad.repository.entity.MessageTimestampEntity;
+import es.unizar.tmdad.repository.entity.RoomEntity;
 import es.unizar.tmdad.service.MessageService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.HttpStatusCodeException;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 @RestController
@@ -15,16 +22,34 @@ import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 @Slf4j
 public class RoomControllerImpl implements RoomController {
     private final MessageService messageService;
+    private final RoomRepository roomRepository;
 
-    public RoomControllerImpl(MessageService messageService){
+    public RoomControllerImpl(MessageService messageService,RoomRepository roomRepository){
         this.messageService = messageService;
+        this.roomRepository = roomRepository;
     }
 
     @Override
     @GetMapping("/{id}")
-    public SseEmitter sendNewTextMessage(@PathVariable("id") Long roomId, @RequestParam("user") String username) {
+    public SseEmitter sendNewTextMessage(@PathVariable("id") Long roomId, @RequestParam("user") String username)  {
         final SseEmitter emitter = new SseEmitter();
         final String topic = "room." + roomId;
+
+        RoomEntity roomEntity = RoomEntity.builder().id(roomId).build();
+        try{
+            if(roomEntity.getUsers().contains(username)){
+                messageService.addSseEmmiter(topic, username, emitter);
+                emitter.onError((callback)->messageService.removeSseEmmiter(topic, username));
+                emitter.onCompletion(()->messageService.removeSseEmmiter(topic, username));
+                emitter.onTimeout(()->messageService.removeSseEmmiter(topic, username));
+            }
+        }catch(HttpClientErrorException.BadRequest exception){
+            throw exception;
+        }
+        log.info("Room {}",topic);
+        return emitter;
+
+        /*
         messageService.addSseEmmiter(topic, username, emitter);
 
         emitter.onError((callback)->messageService.removeSseEmmiter(topic, username));
@@ -33,5 +58,7 @@ public class RoomControllerImpl implements RoomController {
 
         log.info("Room {}", topic);
         return emitter;
+
+         */
     }
 }
